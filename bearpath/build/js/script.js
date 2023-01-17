038e1314131a37ad9b41ae35eb24235c2fbf533a
alekseev konstantin <alexcseev1990@gmail.com>
2023-01-17 16:36:21 +0200

    fix

--- bearpath/build/js/script.js
+++ bearpath/build/js/script.js
@@ -2,26 +2,23 @@ var landingFunctions = {
 	init: function() {
 		this.initLibraris()
 		this.time()
+		this.card()
+		this.modal()
 	}, 
 
 	initLibraris: function() {
 
 		$('[href*="#"]').on('click', function (e) {
-			var fixedOffset = 50;
-			// var cardHeight = $("#card").outerHeight(false)
-			// var windowHeight = $(window).height()
+			var fixedOffset = 0;
+			var cardHeight = $("#card").outerHeight(false)
+			var windowHeight = $(window).height()
 
 			$('html, body')
 				.stop()
-				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
-				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
 			e.preventDefault();
 		});
-
-		$(".footer__color-btn").click(function() {
-			$(".footer__color-btn").removeClass("active")
-			$(this).addClass("active");
-		})
 
 		function initOwl(selector) {
 			var owl = $(selector).owlCarousel({
@@ -30,6 +27,7 @@ var landingFunctions = {
 				nav: true,
 				items: 1,
 				dots: false,
+				autoHeight: true,
 			})
 
 			owl.on("changed.owl.carousel", function(e) {
@@ -39,17 +37,18 @@ var landingFunctions = {
 		}
 
 		initOwl('.galary__slider')
+		initOwl('.card__slider-1')
+		initOwl('.card__slider-2')
+		initOwl('.card__slider-3')
+		initOwl('.review__slider')
 
-		// AOS.init({
-		// 	disable : 'mobile',
-		// 	once: true,
-		// 	duration: 1000,
-		// 	offset : -200,
-		// });
-	
-		// $(window).resize(function() {
-		// 	AOS.refresh();
-		// })
+		$.raty.path = $("body").data("path") +  '/img/raty';
+
+		$('.modal__raiting').raty({
+			half: true,
+			space: false,
+			number: 5,
+		});
 
 		$('[data-fancybox]').fancybox({
 			loop: true,
@@ -82,22 +81,22 @@ var landingFunctions = {
 			};
 		}
 
-		function timer () {
-			function runMultiple(hoursSelector, minutesSelector, secondsSelector, milisecondsSelector) {
-				var d = new Date();
-				var h = String(23 - d.getHours()).padStart(2, "0");
-				var m = String(59 - d.getMinutes()).padStart(2, "0");
-				var s = String(60 - d.getSeconds()).padStart(2, "0");
-				// var ms = String(1000 - d.getMilliseconds()).padStart(3, "0");
-				$(hoursSelector).text(h)
-				$(minutesSelector).text(m)
-				$(secondsSelector).text(s)
-				// $(milisecondsSelector).text(ms)
-			}
-			setInterval(function () {
-				runMultiple(".hours", ".minutes", ".seconds")
-			}, 1000);
-		}
+		// function timer () {
+		// 	function runMultiple(hoursSelector, minutesSelector, secondsSelector, milisecondsSelector) {
+		// 		var d = new Date();
+		// 		var h = String(23 - d.getHours()).padStart(2, "0");
+		// 		var m = String(59 - d.getMinutes()).padStart(2, "0");
+		// 		var s = String(60 - d.getSeconds()).padStart(2, "0");
+		// 		// var ms = String(1000 - d.getMilliseconds()).padStart(3, "0");
+		// 		$(hoursSelector).text(h)
+		// 		$(minutesSelector).text(m)
+		// 		$(secondsSelector).text(s)
+		// 		// $(milisecondsSelector).text(ms)
+		// 	}
+		// 	setInterval(function () {
+		// 		runMultiple(".hours", ".minutes", ".seconds")
+		// 	}, 1000);
+		// }
 	
 		// timer()
 
@@ -126,6 +125,92 @@ var landingFunctions = {
 		}
 
 		$(".date").text(getDate(2))
+	},
+
+	card: function() {
+		function cardImg(selector) {
+			function toggleDataSrcAtribute(string) {
+				$(selector + " .card__slide img").each(function() {
+					$(this)
+						// .hide()
+						.attr("src",  $(this).attr("data-" + string))
+						.parent().attr("href",  $(this).attr("data-" + string))
+						// .fadeIn(1000)
+				})
+			}
+	
+			$(selector + " .card__color-btn").click(function () {
+				var color = $(this).data("color")
+				toggleDataSrcAtribute(color)
+	
+				$(selector + " .card__color-btn").removeClass("active")
+				$(this).addClass("active")
+
+				var price = $(this).data("price")
+				var currency = $(this).data("currency")
+				$(this).closest(".card").find(".new__price").hide().text(Number(price) + " " + currency).fadeIn(600)
+				$(this).closest(".card").find(".old__price").hide().text((Number(price) * 2) + " " + currency).fadeIn(600)
+			})
+		}
+		
+		cardImg(".card__2")
+		cardImg(".card__3")
+	},
+
+	modal: function() {
+		function modal() {
+			$(".add__review").click(function () {
+				$(".modal__review").addClass("active")
+			})
+	
+			function close() {
+				$(".modal__review").removeClass("active")
+			}
+	
+			$(".modal__review").click( function(e) {
+				var target = e.target;
+				if(target.classList.contains("modal__close")) {
+					close()
+				}
+				if(target.classList.contains("modal")) {
+					close()
+				}
+			})
+	
+			function readURL(input) {
+				if (input.files && input.files[0]) {
+					var reader = new FileReader();
+					console.log(reader)
+					reader.onload = function (e) {
+						$('.file img').attr('src', e.target.result).css("display", "block");
+					};
+					reader.readAsDataURL(input.files[0]);
+				}
+			}
+	
+			$(".modal__review .input__file").on("change", function () {
+				readURL(this);
+			});
+	
+			$(".modal__review form").submit(function (e) {
+				e.preventDefault()
+				$(this).removeClass("active");
+				$(".send__window").addClass("active");
+				$(".modal__review .name__input").val("")
+				$(".modal__review .modal__area").val("")
+				$(".modal__review .file img").attr("src", "").css("display", "none")
+				delayClose()
+			})
+			function delayClose() {
+				setTimeout(function () {
+					$(".modal__review form").addClass("active");
+					$(".send__window").removeClass("active");
+					close();
+				}, 5000);
+			}
+		}
+	
+		modal()
 	},
 
 }
